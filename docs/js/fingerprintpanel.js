b65c9c677785008b5fa1f2cb5351784cadcd8408
Anthony Ma <akma327@stanford.edu>
2017-05-12 14:20:00 -0700

    Extensions to fingerprinting

--- docs/js/fingerprintpanel.js
+++ docs/js/fingerprintpanel.js
@@ -4,8 +4,79 @@ var include_nodes = []
 var excludeElems = []
 var exclude_nodes = []
 
+var frameDict;
+var frameIndex_to_collabel
 
-function threeStateSelection(el, row, col, i){
+
+function getFrameDict(contents){
+    // Get mapping from column label to index
+    var content_dict = JSON.parse(contents)
+    if("frameDict" in content_dict){
+        return content_dict["frameDict"];
+    }
+    return;
+}
+
+function getFingerprintColumns(contents){
+    // Get fingerprint column labels
+    var column_labels = []
+    var frameDict = JSON.parse(contents)["frameDict"]
+    for (key in frameDict){
+        column_labels.push(frameDict[key]);
+    }
+    return column_labels;
+}
+
+function updateIntersectFrames(){
+    // Update the UI when user clicks table cell
+    frameIndex_to_collabel = {}
+    for (frameIndex in frameDict){
+        var collabel = frameDict[frameIndex]
+        frameIndex_to_collabel[collabel] = frameIndex
+    }
+
+
+    var include_sel = [];
+    for (var i = 0; i < include_nodes.length; i++){
+        include_sel.push(frameIndex_to_collabel[include_nodes[i]]);
+    }
+
+    var exclude_sel = [];
+    for (var i = 0; i < exclude_nodes.length; i++){
+        exclude_sel.push(frameIndex_to_collabel[exclude_nodes[i]]);
+    }
+
+    if(exclude_sel.length == 0){
+        flareplot.framesIntersect(include_sel);
+    }else{
+        flareplot.framesIntersectSubtract(include_sel, exclude_sel)
+    }
+    
+}
+
+function twoStateSelection(el, row, col){
+    // Allows users to toggle between include or ignore 
+    include_nodes = [];
+
+    var index = includeElems.indexOf(el)
+    if(index >= 0){ 
+        el[0][0].className = "ignore";
+        includeElems.splice(index, 1);
+    } 
+    else{
+        includeElems.push(el)
+    }
+    if(includeElems.length != 0){
+        for (var i = 0; i < includeElems.length; i++){
+            includeElems[i][0][0].className = "include";
+            include_nodes.push(includeElems[i].col_header);
+        }
+    }
+    console.log(include_nodes)
+}
+
+
+function threeStateSelection(el, row, col){
     // Allows user to toggle between include, exclude, and ignore nodes
     
     include_nodes = [];
@@ -29,12 +100,10 @@ function threeStateSelection(el, row, col, i){
 
     for (var i = 0; i < includeElems.length; i++){
         includeElems[i][0][0].className = "include";
-        // include_nodes.push(includeElems[i][0][0].innerText)
         include_nodes.push(includeElems[i].col_header);
     }
     for (var j = 0; j < excludeElems.length; j++){
         excludeElems[j][0][0].className = "exclude";
-        // exclude_nodes.push(excludeElems[j][0][0].innerText)
         exclude_nodes.push(excludeElems[j].col_header);
     }
 
@@ -45,7 +114,6 @@ function threeStateSelection(el, row, col, i){
 function initFingerprintPanel(containerId, columnNames, numRows, callback){
     // Creates the grid and attaches printClick callback to each cell
     var numCols = columnNames.length;
-    var i = 0;
 
     var outerPanel = d3.select(containerId).append('div')
         .attr("class", "fpPanelOuter")
@@ -72,84 +140,114 @@ function initFingerprintPanel(containerId, columnNames, numRows, callback){
         for (var c = 0; c < numCols; ++c){
             var cell = tr.append('td');
             cell.col_header = columnNames[c]; // Attribute to keep track of column header
-            cell.on('click', function(el, r, c, i){
+            cell.on('click', function(el, r, c){
                 return function(){
-                    callback(el, r, c, i);
+                    callback(el, r, c);
                 }
-            }(cell, r, c, i), false);
+            }(cell, r, c), false);
         }
     }
 
     return panel
 }
 
-function twoStateSelection(el, row, col, i){
-    // Allows users to toggle between include or ignore 
-    include_nodes = [];
 
-    var index = includeElems.indexOf(el)
-    if(index >= 0){ 
-        el[0][0].className = "ignore";
-        includeElems.splice(index, 1);
-    } 
-    else{
-        includeElems.push(el)
+function calcFingerprints(contents){
+    function Comparator(a, b) {
+        if (a[1] > b[1]) return -1;
+        if (a[1] < b[1]) return 1;
+        return 0;
     }
-    if(includeElems.length != 0){
-        for (var i = 0; i < includeElems.length; i++){
-            includeElems[i][0][0].className = "include";
-            include_nodes.push(includeElems[i].col_header);
+
+    var fingerprint_dict = {} 
+    var content_dict = JSON.parse(contents);
+    var edges = content_dict["edges"];
+    for (var i = 0; i < edges.length; i++){
+        var e = edges[i];
+        var frames = e["frames"];
+        if(frames.length == 0){
+            continue
+        }
+        var name1 = e["name1"];
+        var name2 = e["name2"];
+        if(!(frames in fingerprint_dict)){
+            fingerprint_dict[frames] = 1;
+        }else{
+            fingerprint_dict[frames] += 1;
         }
     }
-    console.log(include_nodes)
+
+
+    fingerprint_list = [];
+    for (key in fingerprint_dict){
+        fingerprint_list.push([key, fingerprint_dict[key]]);
+    }
+
+    // Sort by frequency
+    fingerprint_list = fingerprint_list.sort(Comparator);
+    p = fingerprint_list;
+    return fingerprint_list;
 }
 
 
-var frameDict;
-var frameIndex_to_collabel
+function threeStateRowSelection(tr, r, columnNames){
+    include_nodes = []
+    exclude_nodes = []
+    var row_cells = tr[0][0].cells;
 
-
-function getFrameDict(contents){
-    var content_dict = JSON.parse(contents)
-    if("frameDict" in content_dict){
-        return content_dict["frameDict"];
+    for (var i = 0; i < row_cells.length; i++){
+        var state = row_cells[i].className;
+        if(state == "include"){
+            include_nodes.push(columnNames[i])
+        }else if(state == "exclude"){
+            exclude_nodes.push(columnNames[i])
+        }
     }
-    return;
 }
 
-function getFingerprintColumns(contents){
-    // Get fingerprint column labels
-    var column_labels = []
-    var frameDict = JSON.parse(contents)["frameDict"]
-    for (key in frameDict){
-        console.log(frameDict[key])
-        column_labels.push(frameDict[key]);
-    }
-    return column_labels;
-}
 
-function updateIntersectFrames(){
-    frameIndex_to_collabel = {}
-    for (frameIndex in frameDict){
-        var collabel = frameDict[frameIndex]
-        frameIndex_to_collabel[collabel] = frameIndex
-    }
+function initScrollableFingerprintPanel(containerId, columnNames, numRows, fingerpint_list, callback){
+    // Creates the grid and attaches printClick callback to each cell
+    var numCols = columnNames.length;
+
+    var panel = d3.select("#fingerprintOuter").append('table')
+        .attr('class', 'fpScrolldown')
+        .attr('id', 'scrolldown')
 
 
-    var include_sel = [];
-    for (var i = 0; i < include_nodes.length; i++){
-        include_sel.push(frameIndex_to_collabel[include_nodes[i]]);
+    // Column Headers
+    var column_header_panel = panel.append("thead").append('tr');
+    var col_headers = []
+    for (var c = 0; c < numCols; c++){
+        var col_header = column_header_panel.append('th').append("span")
+        col_header.html("");
+        col_headers.push("")
     }
 
-    var exclude_sel = [];
-    for (var i = 0; i < exclude_nodes.length; i++){
-        exclude_sel.push(frameIndex_to_collabel[exclude_nodes[i]]);
+
+    // Fingerprint cells
+    for (var r = 0; r < numRows; r++){
+        var fpattern = fingerprint_list[r][0].split(",").map(Number);
+        var count = fingerprint_list[r][1];
+
+        var tr = panel.append("tbody").append('tr');
+        for (var c = 0; c < numCols; c++){
+
+            var cell = tr.append('td')
+            cell.col_header = columnNames[c]; // Attribute to keep track of column header
+            if(fpattern.includes(c)){
+                cell[0][0].className = 'include';
+            }else{
+                cell[0][0].className = 'exclude';
+            }
+        }
+        tr.on('click', function(tr, r, columnNames){
+            return function(){
+                callback(tr, r, columnNames);
+            }
+        }(tr, r, columnNames), false);
+
     }
 
-    if(exclude_sel.length == 0){
-        flareplot.framesIntersect(include_sel);
-    }else{
-        flareplot.framesIntersectSubtract(include_sel, exclude_sel)
-    }
-    
+    return panel
 }
