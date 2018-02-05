b50edf88c53ba000a1cdbfffd74dd4f76b43008f
Rasmus Fonseca <fonseca.rasmus@gmail.com>
2018-02-05 17:24:36 +0100

    Added edgefrequencies script and made minor tweaks to existing input-scripts

--- input/flare-scripts/timeflare_edgefrequencies.py
+++ input/flare-scripts/timeflare_edgefrequencies.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
 """
-Takes a time-flare and returns a single-flare containing only interactions that
-occur more than a specific fraction of time (high-pass).
+Takes a time-flare and returns a single-flare where edges have a weight
+corresponding to their frequency in a specified time-slice of the input. By
+default this time-slice is the entire range.
 
 See https://github.com/GPCRviz/flareplot/tree/master/input for detailed
 definitions of different types of flare files. Briefly, a time-flare is a
 flareplot input file where the `frames` contains multiple integer values.
 Usually this would represent e.g. an interactions presence at certain
 time-points in a simulation. A single-flare is a flareplot input file where the
-value of the `frames` attribute of each edge is `[0]`. In this case the
-interactions retained in the output are those that occur most frequently.
+value of the `frames` attribute of each edge is `[0]`. Here, each interaction
+in the output will also has an associated `width` attribute between 0 and
+--width_max indicating its frequency in the input time-flare.
 """
 
 import json
@@ -36,27 +38,33 @@ def main():
                         type=ap.FileType("w"),
                         help="Single-flare output json. If not specified output is written to stdout")
 
-    parser.add_argument("--min_frequency",
-                        type=float,
-                        default=0.5,
-                        help="Only interactions that occur this frequently are retained [default: 0.5]")
-
     parser.add_argument("--first_frame",
                         type=int,
                         default=0,
-                        help="If only a subset of frames should be considered this value indicates the first frame "
+                        help="If only a subset of frames should be considered this value indicates the first frame"
                              "[default: 0]")
 
     parser.add_argument("--last_frame",
                         type=int,
                         default=-1,
                         help="If only a subset of frames should be considered this value indicates the last frame."
-                             "If the value is negative, then all remaining frames are considered. [default: -1]")
+                             "If the value is negative, then all remaining frames are considered [default: -1].")
+
+    parser.add_argument("--width_max",
+                        type=float,
+                        default=8.0,
+                        help="Edge-widths of the output-json equal their frequency in the specified frame interval "
+                             "multiplied by this scaling factor [default: 8.0]")
+
+    parser.add_argument("--frequency_min",
+                        type=float,
+                        default=0.0,
+                        help="Only edges with frequencies above this value are retained [default: 0.0]")
 
     args = parser.parse_args()
 
     timeflare = read_json(args.input)
-    flatten_frames(timeflare, args.min_frequency, args.first_frame, args.last_frame)
+    flatten_frames(timeflare, args.first_frame, args.last_frame, args.frequency_min, args.width_max)
 
     pretty_json = json.dumps(timeflare, indent=2)
     # Put list-of-numbers on a single line
@@ -74,7 +82,7 @@ def read_json(file):
     return json.loads(file.read())
 
 
-def flatten_frames(flare, min_frequency, first_frame, last_frame):
+def flatten_frames(flare, first_frame, last_frame, min_frequency, width_scale):
     if "edges" not in flare:
         print("No edges in input file")
         exit(1)
@@ -91,8 +99,10 @@ def flatten_frames(flare, min_frequency, first_frame, last_frame):
     # Compress "frames" lists into either [] or [0] depending on their frequency in the specified time-range
     for edge in flare["edges"]:
         frames_in_range = len([f for f in edge["frames"] if first_frame <= f <= last_frame])
-        if frames_in_range >= min_frequency * total_frames:
+        frequency = float(frames_in_range) / total_frames
+        if frequency >= min_frequency:
             edge["frames"] = [0]
+            edge["width"] = (frequency * width_scale)
         else:
             edge["frames"] = []
 
