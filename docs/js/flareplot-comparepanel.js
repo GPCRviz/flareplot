49f886142711be4450ef8c870d001e26b6432c8d
Rasmus Fonseca <fonseca.rasmus@gmail.com>
2018-03-07 17:43:05 -0800

    Headers in place

--- docs/js/flareplot-comparepanel.js
+++ docs/js/flareplot-comparepanel.js
@@ -12,32 +12,45 @@ function createComparePanel(flareplot, cellWidth, containerSelector){
     const div = container.append("div")
         .style("border", "1px dashed gray");
 
-    const headers = div.append("div")
-        .selectAll(".header")
+    const headerAngle = -50;
+
+    const headerDiv = div.append("div")
+        .classed("cp-header", true)
+        .style("position", "relative");
+    const headerCells = headerDiv
+        .selectAll(".cp-headerCell")
         .data(colHeaders)
         .enter()
         .append("div")
-        .classed("header", true)
-        .style("width", cellWidth)
-        .style("height", cellWidth)
+        .classed("cp-headerCell", true)
+        .style("line-height", cellWidth+"px")
         .style("position", "absolute")
-        .style("border", "1px dashed red")
+        .style("bottom", "0px")
+        .style("white-space", "nowrap")
         .style("overflow", "visible")
+        .style("vertical-align", "middle")
+        .style("transform", "rotate("+headerAngle+"deg)")
+        .style("left", "50%")
+        .style("margin-left", function(d,i){ return ((i - numCols / 2) * cellWidth) + "px"; })
         .text(function(d){ return d; });
 
-    function updateLayout(){
-        // Compute container width
-        const containerStyle = window.getComputedStyle(container.node());
-        const containerPadding = parseFloat(containerStyle.paddingLeft) + parseFloat(containerStyle.paddingRight);
-        const containerWidth = parseFloat(container.node().clientWidth) - containerPadding;
+    // Cell width hasn't been set yet, so will represent the width of the text. Compute the max width
+    const maxHeaderCellWidth = d3.max(d3.merge(headerCells), function(c){ return c.clientWidth; });
+    headerCells.style("width", cellWidth+"px");
 
-        console.log(headers);
-        headers
-            .style("left", function(d,i){
-                console.log(d);
-                console.log((containerWidth - numCols * cellWidth) / 2 + i * cellWidth);
-                return (containerWidth - numCols * cellWidth) / 2 + i * cellWidth;
-            })
+    // Use the max text width and header rotation angle to compute and set the real height of the header
+    const headerHeight = Math.sin(Math.abs(headerAngle) * Math.PI / 180) * maxHeaderCellWidth;
+    headerDiv.style("height", headerHeight + "px");
+
+    const bodyDiv = div.append("div")
+        .classed("cp-body", true)
+        .
+
+    function updateLayout(){
+        // // Compute container width
+        // const containerStyle = window.getComputedStyle(container.node());
+        // const containerPadding = parseFloat(containerStyle.paddingLeft) + parseFloat(containerStyle.paddingRight);
+        // const containerWidth = parseFloat(container.node().clientWidth) - containerPadding;
     }
 
     updateLayout();
